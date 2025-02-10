69be075f782993746103ea03eec7c5a7c79496cd
Mario <astorgamhdez@gmail.com>
2025-02-10 12:58:10 -0700

    wip registro

---
+++ Registro.php
@@ -0,0 +1,7 @@
+<form action="verificacion_registro.php">
+<p>Nombre: <input type="text" name="" id=""></p>
+<p>Usuario: <input type="text" name="" id=""></p>
+<p>Contraseña: <input type="text" name="" id=""></p>
+<p>Confirmar contraseña: <input type="text" name="" id=""></p>
+<input type="submit" name="txtBtn1" value="Enviar">
+</form>
--- inicio_sesion.php
+++ inicio_sesion.php
@@ -1,4 +1,4 @@
-<form action="revisar.php" method="POST">
+<form action="verificacion_login.php" method="POST">
     <p>Nombre: <input type="text" name="txtNombre" id=""><br><br></p>
     <p>Usuario: <input type="text" name="txtUsuario" id=""><br><br></p>
     <p>Contraseña: <input type="password" name="txtPassword" id="" ></p>
